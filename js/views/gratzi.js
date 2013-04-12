56d08bffd91adf7e393466f1e5e551ddc1004bd3
Augustin Bralley <augustin@datacosmos.com>
2013-04-12 00:17:17 -0700

    style changes on gratzi

--- js/views/gratzi.js
+++ js/views/gratzi.js
@@ -3,7 +3,7 @@
 
 function gratCallback(json) {
   //$('#gTo').append(json.to);
-  $('#gName').append(json.sender.fullname);
+  $('#gName').append(json.sender.fullname + " has sent you a Grat.");
   $('#gMessage').append(json.message);
   $('#gTags').append(json.tags);
   $('#gImage').attr("src", json.sender.image);
